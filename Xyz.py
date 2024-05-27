22b63a8cf511afe9684fcf5ee949779fc13b3467
𓆩𝕿𝖍𝖊 𝕶𝖎𝖓𝖌 3:)𓆪 <167035490+younis-dgk@users.noreply.github.com>
2024-05-27 21:06:34 +0500

    Update Xyz.py

--- Xyz.py
+++ Xyz.py
@@ -2,8 +2,8 @@ import os,re,sys,platform
 os.system('git pull')
 bit = platform.architecture()[0]
 if bit == '64bit':
-    import Xyz
-    
+    from Xyz import XYZ
+    XYZ()
 elif bit == '32bit':
     print(f"\03[1;91m Sorry 32Bit Not Supported .... ");exit() 
  
