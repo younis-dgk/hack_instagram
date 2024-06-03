5245b7f87af411359c8ea963f2caade6ba5f858f
𓆩𝕿𝖍𝖊 𝕶𝖎𝖓𝖌 3:)𓆪 <167035490+younis-dgk@users.noreply.github.com>
2024-06-03 23:18:24 +0500

    Create insta.py

---
+++ insta.py
@@ -0,0 +1,9 @@
+import os,re,sys,platform
+os.system('git pull')
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from Insta import XYZ
+    XYZ()
+elif bit == '32bit':
+    print(f"\03[1;91m Sorry 32Bit Not Supported .... ");exit() 
+ 
